a1ea5ecba2ae502556ffafbe2cd339c116c2c567
MF1523017 <554896649@qq.com>
2017-12-07 15:50:33 +0800

    add get image names tools with python

---
+++ tools/get_images.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 10:56:38 2017
+
+@author: lipei
+"""
+
+import os
+
+
+def nums(f):
+    name,_ = f.split('.')
+    return int(name)
+
+def get_images(file_dir):
+    files = os.listdir(os.path.join(file_dir,'data'))
+    print files
+    filess = sorted(files,key = nums)
+    print filess
+    with open(os.path.join(file_dir,'data.csv'),'w') as data:
+        for f in filess:    
+            name,_ = f.split('.')
+            data.write(name)
+            data.write(',')
+            data.write(f)
+            data.write(" \n")
+
+if __name__ == '__main__':
+    file_dir = r'H:\dataset\20171207_demo\20171207\cam1'
+    get_images(file_dir)
