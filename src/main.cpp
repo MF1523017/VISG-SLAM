1f9fcd4ee5ffecea34858ac61dc0270d534dce3f
lipei <554896649@qq.com>
2018-05-02 15:39:54 +0800

    add some test

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,11 @@ int main(int argc, char **argv) {
 	}
 	std::cout << "usage: " << argv[0] << " (for running stereo slam) or " << argv[0] << " data_dir (for recording data)" << std::endl;
 #else
-	//test_offline();
+	test_offline();
 	//test_stereo();
 	//test_thread_pool();
-	test_loop();
+	//test_loop();
+	//test_EuRoCDataset();
 #endif
     return 0;
 }
